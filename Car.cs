b115d6f08252c68c9eb96b833b8b9eb0397981e1
Zalvorix <114089240+Zalvorix@users.noreply.github.com>
2023-05-02 09:48:18 -0500

    Initial commit

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Babcock_Olivia
+{   // Creates a class
+    class Car
+    
+    {/// <summary>
+    /// Creates setter and getter for the model property
+    /// </summary>
+        private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Properties_Babcock_Olivia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Creates a car
+            Car car = new Car();
+            // Assigns a model and make to the car
+            car.Make = "Toyota";
+            car.Model = "Tundra";
+
+            // Prints model and make to the console
+            Console.WriteLine($"Make: {car.Make}, Model: {car.Model}");
+
+            // Creates a car
+            Car car2 = new Car();
+            // Assigns a model and make to the car
+            car2.Make = "Infiniti";
+            car2.Model = "SUV";
+
+            // Prints model and make to the console
+            Console.WriteLine($"Make: {car2.Make}, Model: {car2.Model}");
+
+            // Creates a car
+            Car car3 = new Car();
+
+            // Prints model and make to the console
+            Console.WriteLine($"Make: {car3.Make}, Model: {car3.Model}");
+        }
+    }
+}
